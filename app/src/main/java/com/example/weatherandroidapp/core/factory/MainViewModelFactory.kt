7d7d5b959a6369e9943d647277f8351cebdb138b
Darina <37955869+dagenator@users.noreply.github.com>
2022-07-20 11:41:11 +0500

    little refactoring

--- app/src/main/java/com/example/weatherandroidapp/core/factory/MainViewModelFactory.kt
+++ app/src/main/java/com/example/weatherandroidapp/core/factory/MainViewModelFactory.kt
@@ -4,17 +4,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherandroidapp.data.repository.MainRepository
 import com.example.weatherandroidapp.presenter.MainViewModel
+import com.example.weatherandroidapp.utils.WeatherStateUtil
 import javax.inject.Inject
 
 @Suppress("UNCHECKED_CAST")
-class MainViewModelFactory @Inject constructor(val repository: MainRepository) :
+class MainViewModelFactory @Inject constructor(val repository: MainRepository, var weatherStateUtil: WeatherStateUtil) :
     ViewModelProvider.NewInstanceFactory() {
 
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
 
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            return MainViewModel(repository) as T
+            return MainViewModel(repository,weatherStateUtil) as T
         }
 
         throw IllegalArgumentException("Unknown ViewModel class")
--- app/src/main/java/com/example/weatherandroidapp/presenter/MainViewModel.kt
+++ app/src/main/java/com/example/weatherandroidapp/presenter/MainViewModel.kt
@@ -10,10 +10,11 @@ import com.example.weatherandroidapp.data.models.Result
 import com.example.weatherandroidapp.data.models.UVInfo
 import com.example.weatherandroidapp.data.repository.MainRepository
 import com.example.weatherandroidapp.utils.Resource
+import com.example.weatherandroidapp.utils.WeatherStateUtil
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(val repository: MainRepository) : ViewModel() {
+class MainViewModel @Inject constructor(val repository: MainRepository, val weather: WeatherStateUtil) : ViewModel() {
 
     val currentWeatherLiveData: MutableLiveData<Resource<CurrentWeather>> = MutableLiveData(null)
     val errorLiveData: MutableLiveData<String> = MutableLiveData(null)
@@ -34,79 +35,9 @@ class MainViewModel @Inject constructor(val repository: MainRepository) : ViewMo
             }
         }
     }
+    fun UVDescription(UV :Int) = weather.UVDescription(UV)
 
-    fun UVDescription(UV :Int):String{
-        when(UV){
-            in 0..2-> return "УФ Низкий"
-            in 3..5-> return "УФ Средний. Рекомендуется использовать крем c spf"
-            in 6..7-> return "УФ Высокий. Не забудьте про крем c spf"
-            in 8..10-> return "УФ Очень высокий. Используйте крем c spf"
-            else-> return "УФ Экстремальный. Апокалипсис случился. "
-        }
-    }
+    fun getImageStateSet(weatherId: Int)= weather.getImageStateSet(weatherId)
 
-    fun getImageStateSet(weatherId: Int): ImageStateSet {
 
-        when (weatherId) {
-            in 200..299 -> return ImageStateSet(
-                weatherId,
-                R.drawable.thunderstorm,
-                R.drawable.ic_thunderstorm_icon
-            )
-            300, 301, 310, 311, 500, 501 -> return ImageStateSet(
-                weatherId,
-                R.drawable.rain,
-                R.drawable.ic_rain_icon
-            )
-            in 300..399 -> return ImageStateSet(
-                weatherId,
-                R.drawable.heavy_rain,
-                R.drawable.ic_heavy_rain_icon
-            )
-            511, in 611..616 -> return ImageStateSet(
-                weatherId,
-                R.drawable.freezing_rain,
-                R.drawable.ic_frezing_rain_icon
-            )
-            in 520..531 -> return ImageStateSet(
-                weatherId,
-                R.drawable.heavy_rain,
-                R.drawable.ic_heavy_rain_icon
-            )
-            in 600..601, 620 -> return ImageStateSet(
-                weatherId,
-                R.drawable.snow,
-                R.drawable.ic_snow_icon
-            )
-            602, 621, 622 -> return ImageStateSet(
-                weatherId,
-                R.drawable.snow,
-                R.drawable.ic_heavy_snow
-            )
-            in 700..799 -> return ImageStateSet(weatherId, R.drawable.mist, R.drawable.ic_mist_icon)
-            800 -> return ImageStateSet(
-                weatherId,
-                R.drawable.clear_sky,
-                R.drawable.ic_clear_sky_icon
-            )
-            801 -> return ImageStateSet(
-                weatherId,
-                R.drawable.few_clouds,
-                R.drawable.ic_few_clouds_icon
-            )
-            802 -> return ImageStateSet(
-                weatherId,
-                R.drawable.scattered_clouds,
-                R.drawable.ic_scattered_clouds_icon
-            )
-            803 -> return ImageStateSet(weatherId, R.drawable.clouds, R.drawable.ic_clouds_icon)
-            804 -> return ImageStateSet(
-                weatherId,
-                R.drawable.heavy_clouds,
-                R.drawable.ic_heavy_clouds_icon
-            )
-            else -> return ImageStateSet(weatherId, R.drawable.night_sky, R.drawable.ic_mist_icon)
-        }
-
-    }
 }
